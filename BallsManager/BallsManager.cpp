0da08d345b4fd198d090cbdac1beca7359928e18
Aleksandr Rybalka <polterageist@gmail.com>
2023-08-24 04:10:31 +0300

    Lesson-1-2-4

---
+++ BallsManager/BallsManager.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+const int NUM_BALLS = 20;
+
+int main()
+{
+	int balls[NUM_BALLS];
+	for (int& ball : balls)
+	{
+		ball = 0;
+	}
+
+	bool shouldContinue = true;
+	while (shouldContinue)
+	{
+		std::cout << "List of commands:" << std::endl;
+		std::cout << "0 - Exit" << std::endl;
+		std::cout << "1 - Add ball" << std::endl;
+		std::cout << "2 - Remove ball" << std::endl;
+		std::cout << "3 - Count balls" << std::endl;
+		std::cout << "4 - Total balls" << std::endl;
+		std::cout << "5 - Needed baals" << std::endl;
+		std::cout << ">: ";
+		std::string command;
+		std::cin >> command;
+
+		if (command == "1")
+		{
+
+		}
+		else if (command == "2")
+		{
+
+		}
+		else if (command == "3")
+		{
+			std::cout << "Enter article: ";
+			int article = -1;
+			std::cin >> article;
+			if (article < 0 || article > NUM_BALLS - 1)
+			{
+				std::cout << "Incorrect article!" << std::endl;
+			}
+			else
+			{
+				std::cout << "There are " << balls[article] << " balls of article " << article << std::endl;
+			}
+		}
+		else if (command == "4")
+		{
+
+		}
+		else if (command == "5")
+		{
+			for (int i = 0; i < NUM_BALLS; ++i)
+			{
+				if (balls[i] == 0)
+				{
+					std::cout << "There are no balls of article " << i << std::endl;
+				}
+			}
+		}
+		else if (command == "0")
+		{
+			std::cout << "Bye!" << std::endl;
+			break;
+		}
+		else
+		{
+			std::cout << "Unknown command " << command << std::endl;
+		}
+	}
+}
--- PolishCalc/PolishCalc.cpp
+++ PolishCalc/PolishCalc.cpp
@@ -2,33 +2,42 @@
 
 int main()
 {
-	std::cout << "Enter operation (+ - / *) and operands in format op a b!\n";
-	std::string operation;
-	float a = 0.f;
-	float b = 0.f;
-	std::cin >> operation >> a >> b;
-	float result = 0.f;
-	if (operation == "+")
+	bool shouldContinue = true;
+	std::string answer;
+	while (shouldContinue)
 	{
-		result = a + b;
-	}
-	else if (operation == "-")
-	{
-		result = a - b;
-	}
-	else if (operation == "*")
-	{
-		result = a * b;
-	}
-	else if (operation == "/")
-	{
-		result = a / b;
-	}
-	else
-	{
-		std::cout << "Unknown operation: " << operation << "\n";
-		return 0;
-	}
+		std::cout << "Enter operation (+ - / *) and operands in format op a b!\n";
+		std::string operation;
+		float a = 0.f;
+		float b = 0.f;
+		std::cin >> operation >> a >> b;
+		float result = 0.f;
+		if (operation == "+")
+		{
+			result = a + b;
+		}
+		else if (operation == "-")
+		{
+			result = a - b;
+		}
+		else if (operation == "*")
+		{
+			result = a * b;
+		}
+		else if (operation == "/")
+		{
+			result = a / b;
+		}
+		else
+		{
+			std::cout << "Unknown operation: " << operation << "\n";
+			return 0;
+		}
 
-	std::cout << "Result: " << result << "\n";
+		std::cout << "Result: " << result << "\n";
+
+		std::cout << "Do you want to continue? y/n: ";
+		std::cin >> answer;
+		shouldContinue = answer == "y";
+	}
 }

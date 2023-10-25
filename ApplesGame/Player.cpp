6678ec388a6295767c60842394f34a1c476674ca
Aleksandr Rybalka <polterageist@gmail.com>
2023-10-25 20:47:07 +0200

    1-4 Homework part 2: added player sprite rotation accordingly to movement direction

--- ApplesGame/Player.cpp
+++ ApplesGame/Player.cpp
@@ -20,6 +20,18 @@ namespace ApplesGame
 	{
 		player.sprite.setPosition(player.position.x, player.position.y);
 		window.draw(player.sprite);
+
+		// Orient player sprite according to player direction
+		if (player.direction == PlayerDirection::Left)
+		{
+			SetSpriteSize(player.sprite, -PLAYER_SIZE, PLAYER_SIZE);
+			player.sprite.setRotation(0.f);
+		}
+		else
+		{
+			SetSpriteSize(player.sprite, PLAYER_SIZE, PLAYER_SIZE);
+			player.sprite.setRotation((float)player.direction * -90.f);
+		}
 	}
 }
 
